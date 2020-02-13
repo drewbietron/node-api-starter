8f98315918d8962af903695c27b79162688bc0d5
Drew Templeton <drew@drewtempleton.com>
2020-02-13 11:13:51 -0600

    Remove dist directory from github

--- dist/src/database/database.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const config_1 = require("./config");
-exports.database = new sequelize_1.Sequelize(config_1.config.url, Object.assign({}, config_1.configOptions));
-exports.default = exports.database;
